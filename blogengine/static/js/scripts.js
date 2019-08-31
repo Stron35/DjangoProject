ac2762db875398f0fb03c456bede64dea560dd5a
Stron35 <fedorov_98@list.ru>
2019-09-01 00:18:51 +0300

    Restruct for bootstrap4

--- blogengine/static/js/scripts.js
+++ blogengine/static/js/scripts.js
@@ -1,12 +1,5 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const mb = document.querySelectorAll('.materialboxed');
-// 	M.Materialbox.init(mb, {});
-//   });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const carousel = document.querySelectorAll('.carousel');
-    M.Carousel.init( carousel, {
-    	indicators: true,
-    	fullWidth: true
-  	});
-  });
+$(document).ready(function(){
+        $("#carousel").carousel({
+        	interval: 2000
+        });
+});
--- staticfiles/js/scripts.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mb = document.querySelectorAll('.materialboxed');
-	M.Materialbox.init(mb, {});
-  });
