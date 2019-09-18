1665097fc37c19bad9f853338171a421dcdf4f79
Stron35 <fedorov_98@list.ru>
2019-09-18 23:21:32 +0300

    Added 10MB upload file

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,8 +132,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+    'blogengine/static/css/',]
   
 
 STATIC_URL = '/static/'
@@ -142,3 +144,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+#Files
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
